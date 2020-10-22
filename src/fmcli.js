a81f72d68f141b48377c94db9cd541adb6fbd8f8
Woody Wu <Woody.Wu2@landisgyr.com>
2020-10-22 21:04:28 +0800

    don't allow save to block < min level1 block

--- src/fmcli.js
+++ src/fmcli.js
@@ -184,9 +184,11 @@ const timeSpan = argv => {
         model.stop();
         const timeEnd = new Date();
         if (err) console.error(err);
-        console.log('from: ' + dateformat(minTime, 'UTC:yyyy-mm-dd HH:MM Z'));
-        console.log('to:   ' + dateformat(maxTime, 'UTC:yyyy-mm-dd HH:MM Z'));
-        console.log('used ' + (timeEnd - timeStart) / 1000 + 's');
+        if (minTime && maxTime) { /* the device may not have */
+            console.log('from: ' + dateformat(minTime, 'UTC:yyyy-mm-dd HH:MM Z'));
+            console.log('to:   ' + dateformat(maxTime, 'UTC:yyyy-mm-dd HH:MM Z'));
+            console.log('used ' + (timeEnd - timeStart) / 1000 + 's');
+        }
     });
 }
 
